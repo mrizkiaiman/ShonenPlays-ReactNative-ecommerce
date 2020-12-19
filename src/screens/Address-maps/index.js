a560c2d505b0a8dd7d31088951f735defbb54c9a
ashirogisen <m.rizki.aiman@gmail.com>
2020-12-19 15:44:07 +0700

    Maps for address is implemented

--- src/screens/Address-maps/index.js
+++ src/screens/Address-maps/index.js
@@ -1,17 +1,66 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Text, View} from 'react-native'
 //Styling
 import styles from './style'
 import {Size} from '../../style'
 import {tailwind} from '../../style/tailwind'
-//Assets
 //Components
+import {InputMap, ChooseLocationButton} from './components'
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'
+import * as Location from 'expo-location'
 //Functions
 
 export default () => {
+  const [location, setLocation] = useState({})
+
+  useEffect(() => {
+    getCurrentLocation()
+  }, [])
+
+  const getCurrentLocation = async () => {
+    let {status} = await Location.requestPermissionsAsync()
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied')
+      return
+    }
+
+    let location = await Location.getCurrentPositionAsync({})
+    setLocation(location)
+  }
+
+  const changeCurrentLocation = (loc) => {
+    setLocation({
+      latitude: loc.result.geometry.location.lat,
+      longitude: loc.result.geometry.location.lng,
+      latitudeDelta: 0.003,
+      longitudeDelta: 0.003,
+    })
+  }
+
   return (
-    <View style={styles.mainContainer}>
-      <Text></Text>
+    <View>
+      <InputMap notifyChange={changeCurrentLocation} />
+      <MapView
+        showsUserLocation
+        showsIndoorLevelPicker
+        followsUserLocation
+        style={{width: '100%', height: '100%'}}
+        region={location}
+        onRegionChangeComplete={(region) => {
+          setLocation({
+            ...location,
+            latitude: region.latitude,
+            longitude: region.longitude,
+          })
+        }}>
+        <Marker
+          draggable
+          coordinate={location}
+          title="Your Location"
+          description="Chosen Location"
+        />
+      </MapView>
+      <ChooseLocationButton navigate={() => navigateScreen()} />
     </View>
   )
 }
--- src/screens/Address-maps/style.js
+++ src/screens/Address-maps/style.js
@@ -4,5 +4,14 @@ import {tailwind} from '../../style/tailwind'
 
 const {width, height} = Size
 export default StyleSheet.create({
-  mainContainer: {},
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width,
+    height,
+  },
 })
