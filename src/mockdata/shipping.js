1d558844394cbc0e9eec9abca165a53727c6885f
ashirogisen <m.rizki.aiman@gmail.com>
2021-01-29 20:53:16 +0700

    Add shipping mockdata

---
+++ src/mockdata/shipping.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    name: 'JNE - YES (1 Hari)',
+    price: {
+      java: 20000,
+      non_java: 40000,
+    },
+  },
+  {
+    name: 'JNE - REG (2-4 Hari)',
+    price: {
+      java: 15000,
+      non_java: 30000,
+    },
+  },
+  {
+    name: 'SiCepat - BEST (1 Hari)',
+    price: {
+      java: 20000,
+      non_java: 40000,
+    },
+  },
+  {
+    name: 'SiCepat - REG (1 Hari)',
+    price: {
+      java: 15000,
+      non_java: 30000,
+    },
+  },
+]
