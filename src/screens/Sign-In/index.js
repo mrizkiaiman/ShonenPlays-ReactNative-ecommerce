6c47f712349f36ed205b9042a4f7516e09a273ca
ashirogisen <m.rizki.aiman@gmail.com>
2021-02-24 09:49:09 +0700

    Make a separate file for form validation

--- src/screens/Sign-In/index.js
+++ src/screens/Sign-In/index.js
@@ -10,60 +10,29 @@ import {Formik, useFormikContext} from 'formik'
 import * as Yup from 'yup'
 import {Input, Button} from '../../components'
 //Functions
-import {Toast, isEmail} from '../../utils'
+import {Toast, FormValidation} from '../../utils'
 import {addUser} from '../../store/actions/users'
 import {tailwind} from '../../style/tailwind'
-
-const validationSchema = Yup.object().shape({
-  email: Yup.string().required().email().label('Email'),
-  password: Yup.string().required().min(4).max(30).label('Password'),
-})
 
 export default function SignInScreen({navigation}) {
   const [showPassword, setShowPassword] = useState(false)
   const usersFromRedux = useSelector((state) => state.users.data)
 
   const dispatch = useDispatch()
-  const signInOnSubmit = () => {
+  const signInOnSubmit = (email, password) => {
     const isVerified = usersFromRedux.some(
       (user) => user.email == email && user.password == password,
     )
     if (isVerified) {
       Toast({title: 'Success', text: 'Logged in'})
       navigation.navigate('BottomTabs', {screen: 'Home'})
-    } else if (isEmail(email) == false)
-      Toast({title: 'Failed', text: 'Incorrect email format', type: 'error'})
-    else
-      Toast({title: 'Failed', text: 'Wrong email or password', type: 'error'})
+    }
   }
 
   const googleSignIn = async () => {
     Toast({title: 'Success', text: 'Logged in'})
     navigation.navigate('BottomTabs', {screen: 'Home'})
   }
-
-  const buttonList = [
-    {
-      onSubmit: () => signInOnSubmit(),
-      styling: {
-        buttonStyle: styles.signInButton,
-        textStyle: styles.signInButtonText,
-      },
-      title: 'Sign in',
-    },
-    {
-      onSubmit: () => googleSignIn(),
-      styling: {
-        buttonStyle: styles.connectWithGoogleButton,
-        textStyle: styles.connectWithGoogleButtonText,
-      },
-      title: 'Connect with Google',
-      additionalComponents: {
-        position: 'left',
-        comps: <GoogleIcon />,
-      },
-    },
-  ]
 
   return (
     <SafeAreaView style={styles.safeAreaContainer}>
@@ -82,8 +51,8 @@ export default function SignInScreen({navigation}) {
         </View>
         <Formik
           initialValues={{email: '', password: ''}}
-          onSubmit={(value) => console.log(value)}
-          validationSchema={validationSchema}>
+          onSubmit={() => signInOnSubmit(email, password)}
+          validationSchema={FormValidation.Login}>
           {({handleChange, handleSubmit, errors}) => (
             <>
               <View style={styles.inputsContainer}>
@@ -117,9 +86,23 @@ export default function SignInScreen({navigation}) {
                   <Text style={{color: 'red'}}>{errors.password}</Text>
                 )}
               </View>
-              {buttonList.map((button, index) => (
-                <Button key={index} {...button} />
-              ))}
+              <Button
+                onSubmit={handleSubmit}
+                styling={{
+                  buttonStyle: styles.signInButton,
+                  textStyle: styles.signInButtonText,
+                }}
+                title="Sign in"
+              />
+              <Button
+                onSubmit={() => googleSignIn()}
+                styling={{
+                  buttonStyle: styles.connectWithGoogleButton,
+                  textStyle: styles.connectWithGoogleButtonText,
+                }}
+                title="Connect with Google"
+                additionalComponents={{position: 'left', comps: <GoogleIcon />}}
+              />
             </>
           )}
         </Formik>
